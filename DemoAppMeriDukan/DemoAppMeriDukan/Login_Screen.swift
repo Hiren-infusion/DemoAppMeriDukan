30fd5451003b152bf01caea262f42301ebd966c8
Deep Panwala <deepjp@infusionanalysts.com>
2021-08-13 20:32:22 +0530

    Create Welcome Screen

---
+++ DemoAppMeriDukan/DemoAppMeriDukan/Login_Screen.swift
@@ -0,0 +1,20 @@
+//
+//  Login_Screen.swift
+//  DemoAppMeriDukan
+//
+//  Created by iOS Dev2 on 13/08/21.
+//
+
+import SwiftUI
+
+struct Login_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Screen_Previews: PreviewProvider {
+    static var previews: some View {
+        Login_Screen()
+    }
+}
---
+++ DemoAppMeriDukan/DemoAppMeriDukan/SignUp_Screen.swift
@@ -0,0 +1,20 @@
+//
+//  SignUp_Screen.swift
+//  DemoAppMeriDukan
+//
+//  Created by iOS Dev2 on 13/08/21.
+//
+
+import SwiftUI
+
+struct SignUp_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Screen_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp_Screen()
+    }
+}
